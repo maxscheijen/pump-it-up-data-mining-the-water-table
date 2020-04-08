7ae029c194a24e31cc1313d257c948c1f6050083
Max Scheijen <maxscheijen@gmail.com>
2020-04-08 14:10:26 +0200

    added type hints and docs to label encoding

--- src/features/categorical_encoding.py
+++ src/features/categorical_encoding.py
@@ -1,7 +1,19 @@
+import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 
-def label_encoding(df):
+def label_encoding(df: pd.DataFrame) -> pd.DataFrame:
+    """Label encoding of categorical features.
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+    
+    Returns
+    -------
+    pd.DataFrame
+        dataframe with categorical variables label encoded.
+    """
     cat_cols = df.select_dtypes('O').columns.values
     df[cat_cols] = df[cat_cols].apply(LabelEncoder().fit_transform)
     return df
